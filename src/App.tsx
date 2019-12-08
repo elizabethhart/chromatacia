9186fc882facfe9845d779f682d67e36553c5486
elizabethhart <elizabethwhart@gmail.com>
2019-12-08 15:50:04 -0600

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -6,18 +6,12 @@ const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <div className="roygbv"></div>
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          chromatacia
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="App-logo">
+        </div>
       </header>
     </div>
   );
