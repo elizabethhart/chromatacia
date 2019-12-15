df314d13b38cdf4913f0c9d9a464753af8c855bb
elizabethhart <elizabethwhart@gmail.com>
2019-12-15 15:44:05 -0600

    Replace components in about

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,66 +1,44 @@
 import React from "react";
-import {Avatar, Button, Heading, Icon, Pane, Paragraph, Text, UnorderedList, ListItem} from "evergreen-ui";
+import { Button, Icon } from "evergreen-ui";
+import { Card, Col, Container, Row } from 'react-bootstrap';
 import './About.scss';
 
 export default class About extends React.Component {
     render() {
         return <>
             <div className="about color-bar"></div>
-            <Pane clearfix>
-                <Pane
-                    elevation={1}
-                    float="left"
-                    background="overlay"
-                    margin={50}
-                    padding={100}
-                    height={500}
-                    display="flex"
-                    justifyContent="center"
-                    alignItems="center"
-                    flexDirection="column"
-                    borderRadius={10}
-                >
-                    <Avatar
-                        src="https://www.refinery29.com/images/8946936.jpg?format=webp&width=720&height=864&quality=85"
-                        name="Elizabeth Hart"
-                        size={150}
-                    />
-                    <div
-                        className="info-wrapper"
-                    >
-                        <Heading size={500} color="lightblue"> Elizabeth Hart</Heading>
-                        <Paragraph size={300} color="white">Software Engineer</Paragraph>
-                        <Paragraph size={300} color="white">Chicago, IL</Paragraph>
-                        <Button
-                            is="a"
-                            href="mailto:elizabethwhart@example.com"
-                            appearance="minimal"
-                        >
-                            Email &nbsp; <Icon icon="envelope" color="info" />
-                        </Button>
-                    </div>
-                </Pane>
-                <Pane
-                    elevation={1}
-                    float="left"
-                    background="overlay"
-                    margin={50}
-                    padding={10}
-                    height={500}
-                    width={500}
-                    display="flex"
-                    justifyContent="center"
-                    alignItems="center"
-                    flexDirection="column"
-                    borderRadius={10}
-                >
-                    <Heading
-                        color="lightblue"
-                    >
-                        New Pane
-                    </Heading>
-                </Pane>
-            </Pane>
+            <Container className="about-container">
+                <Row>
+                    <Col md="3"></Col>
+                    <Col md="3">
+                        <Card style={{ width: '100%' }}>
+                            <Card.Img variant="top" src="https://www.refinery29.com/images/8946936.jpg?format=webp&width=720&height=864&quality=85" />
+                            <Card.Body>
+                                <Card.Title>Elizabeth Hart</Card.Title>
+                                <Card.Text>
+                                    <p>Software Engineer</p>
+                                    <p>Chicago, IL</p>
+                                    <Button
+                                        is="a"
+                                        href="mailto:elizabethwhart@example.com"
+                                        appearance="minimal"
+                                    >
+                                        Email &nbsp; <Icon icon="envelope" color="info" />
+                                    </Button>
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                    <Col md="3">
+                        <Card className="card-test">
+                            <Card.Title>
+                                New Card
+                            </Card.Title>
+                        </Card>
+                    </Col>
+                    <Col md="3"></Col>
+                </Row>
+            </Container>
         </>
     }
 }
