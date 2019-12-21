a0a9cb720d6abed8a78acb0dfa2224826a538cc4
elizabethhart <elizabethwhart@gmail.com>
2019-12-20 21:09:45 -0600

    User form and list placeholders

---
+++ src/components/User/UserForm.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Button, Form } from 'react-bootstrap';
+
+export default class UserForm extends React.Component {    
+    handleChange(event: any) {}
+    
+    handleSubmit(event: any) {}
+    
+    render() {
+        return (
+            <></>
+        );
+    }
+}
