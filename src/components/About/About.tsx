8e75602bed018d4d2be0441cf5da94dcb2365d7e
elizabethhart <elizabethwhart@gmail.com>
2020-09-04 18:11:31 -0500

    Styled components

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from "react";
 import { Col, Container, Row } from 'react-bootstrap';
 import axios from 'axios';
-
+import styled from 'styled-components';
+import { googleMapsUrl } from "../../constants";
+import { FaGithub, FaMailBulk } from 'react-icons/fa';
 import './About.scss';
 
 type AboutProps = { }
@@ -9,60 +11,103 @@ type AboutProps = { }
 const About: React.FC<AboutProps> = ({
 
 }: AboutProps) => {
-    const [backgroundUrl, setBackgroundUrl] = useState<string>("");
+  const [backgroundUrl, setBackgroundUrl] = useState<string>("");
+  const profileLinks = [
+    {
+      url: 'https://github.com/elizabethhart',
+      text: 'github',
+      icon: <FaGithub />
+    },
+    {
+      url: 'mailto:elizabethwhart@example.com',
+      text: 'email',
+      icon: <FaMailBulk />
+    }
+  ];
 
-    useEffect(() => {
-        let requestUrl = 'https://maps.googleapis.com/maps/api/staticmap?center=Chicago,IL'
-        requestUrl += '&zoom=10&format=png&maptype=roadmap&style=element:geometry%7Ccolor:0x1d2c4d'
-        requestUrl += '&style=element:labels%7Cvisibility:off&style=element:labels.text.fill%7Ccolor:0x8ec3b9'
-        requestUrl += '&style=element:labels.text.stroke%7Ccolor:0x1a3646&style=feature:administrative%7Celement:geometry%7Cvisibility:off'
-        requestUrl += '&style=feature:administrative.country%7Celement:geometry.stroke%7Ccolor:0x4b6878&style=feature:administrative.land_parcel%7Cvisibility:off'
-        requestUrl += '&style=feature:administrative.land_parcel%7Celement:labels.text.fill%7Ccolor:0x64779e&style=feature:administrative.neighborhood%7Cvisibility:off'
-        requestUrl += '&style=feature:administrative.province%7Celement:geometry.stroke%7Ccolor:0x4b6878&style=feature:landscape.man_made%7Celement:geometry.stroke%7Ccolor:0x334e87'
-        requestUrl += '&style=feature:landscape.natural%7Celement:geometry%7Ccolor:0x023e58&style=feature:poi%7Cvisibility:off&style=feature:poi%7Celement:geometry%7Ccolor:0x283d6a'
-        requestUrl += '&style=feature:poi%7Celement:labels.text.fill%7Ccolor:0x6f9ba5&style=feature:poi%7Celement:labels.text.stroke%7Ccolor:0x1d2c4d'
-        requestUrl += '&style=feature:poi.park%7Celement:geometry.fill%7Ccolor:0x023e58&style=feature:poi.park%7Celement:labels.text.fill%7Ccolor:0x3C7680'
-        requestUrl += '&style=feature:road%7Celement:geometry%7Ccolor:0x304a7d&style=feature:road%7Celement:labels.icon%7Cvisibility:off'
-        requestUrl += '&style=feature:road%7Celement:labels.text.fill%7Ccolor:0x98a5be&style=feature:road%7Celement:labels.text.stroke%7Ccolor:0x1d2c4d'
-        requestUrl += '&style=feature:road.highway%7Celement:geometry%7Ccolor:0x2c6675&style=feature:road.highway%7Celement:geometry.stroke%7Ccolor:0x255763'
-        requestUrl += '&style=feature:road.highway%7Celement:labels.text.fill%7Ccolor:0xb0d5ce&style=feature:road.highway%7Celement:labels.text.stroke%7Ccolor:0x023e58'
-        requestUrl += '&style=feature:transit%7Cvisibility:off&style=feature:transit%7Celement:labels.text.fill%7Ccolor:0x98a5be'
-        requestUrl += '&style=feature:transit%7Celement:labels.text.stroke%7Ccolor:0x1d2c4d&style=feature:transit.line%7Celement:geometry.fill%7Ccolor:0x283d6a'
-        requestUrl += '&style=feature:transit.station%7Celement:geometry%7Ccolor:0x3a4762&style=feature:water%7Celement:geometry%7Ccolor:0x0e1626'
-        requestUrl += '&style=feature:water%7Celement:labels.text.fill%7Ccolor:0x4e6d70&size=480x360'
-        requestUrl += '&key=' + process.env.REACT_APP_GOOGLE_MAPS_API_KEY
+  useEffect(() => {
+    let requestUrl = googleMapsUrl + process.env.REACT_APP_GOOGLE_MAPS_API_KEY
 
-        axios.get(requestUrl)
-            .then((res: any) => {
-                setBackgroundUrl(res.config.url);
-            });
-    }, []);
+    axios.get(requestUrl)
+      .then((res: any) => {
+        setBackgroundUrl(res.config.url);
+      })
+      .catch((error: any) => {
+        console.log('error', error);
+      });
+  }, []);
 
-    return (
-        <>
-            <div className="about color-bar"></div>
-            <Container className="about-container" style={{backgroundImage: `url(${backgroundUrl})`}}>
-                <Row>
-                    <Col md="3"></Col>
-                    <Col md="6">
-                        <div className="card-theme">
-                            <img className="profile-img" alt="google-map" src="https://elizabethhart.github.io/images/IMG_1321.JPG" />
-                            <div className="profile-content">
-                                <h1>Liz Hart</h1>
-                                <div>
-                                    <p>Software Engineer</p>
-                                    <p>Chicago, IL</p>
-                                    <p><a href="https://github.com/elizabethhart">github&nbsp;<i className="fa fa-github"></i></a></p>
-                                    <p><a href="mailto:elizabethwhart@example.com">email&nbsp;<i className="fa fa-envelope"></i></a></p>
-                                </div>
-                            </div>
-                        </div>
-                    </Col>
-                    <Col md="3"></Col>
-                </Row>
-            </Container>
-        </>
-    );
+  const Card = styled.div`
+    background-color: #202020;
+    border-radius: 5px;
+    padding: 10px;
+    height: 300px;
+    margin-top: 50px;
+  `;
+
+  const ProfileImage = styled.img`
+    display: inline-block;
+    width: 40%;
+    border-radius: 50%;
+    margin: 20px;
+    vertical-align: top;
+  `;
+
+  const ProfileContent = styled.div`
+    display: inline-block;
+    width: 40%;
+  `;
+
+  const ProfileHeader = styled.h1`
+    color: #7DFDFE;
+  `;
+
+  const ProfileSubheader = styled.h6`
+    color: white;
+  `;
+
+  const ProfileLink = styled.a`
+    color: white;
+    display: block;
+  `;
+
+  return (
+    <>
+      <div className="about color-bar"></div>
+      <Container 
+        className="about-container" 
+        style={{backgroundImage: `url(${backgroundUrl})`}}
+      >
+        <Row>
+          <Col md="3"></Col>
+          <Col md="6">
+            <Card>
+              <ProfileImage
+                alt="google-map" 
+                src="https://elizabethhart.github.io/images/IMG_1321.JPG" 
+              />
+              <ProfileContent>
+                <ProfileHeader>
+                  Liz Hart
+                </ProfileHeader>
+                <ProfileSubheader>
+                  Software Engineer | Artist
+                </ProfileSubheader>
+                {profileLinks.map((profileLink) => {
+                  return <ProfileLink
+                    href={profileLink.url}
+                  >
+                    {profileLink.icon} {profileLink.text}
+                  </ProfileLink>
+                })}
+              </ProfileContent>
+            </Card>
+          </Col>
+          <Col md="3"></Col>
+        </Row>
+      </Container>
+    </>
+  );
 };
 
 export default About;
--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,23 +1,33 @@
 import React from "react";
+import { useHistory } from 'react-router-dom';
 import { Heading } from "evergreen-ui";
 import { Nav, Navbar } from 'react-bootstrap';
 import ColorWheel from "../ColorWheel/ColorWheel";
 import './Navigation.scss';
 
-export default class Navigation extends React.Component {
-    render() {
-        return <>
-            <Navbar bg="dark" expand="lg" variant="dark">
-                <Navbar.Brand href="/">
-                    <Heading size={600}>
-                        <ColorWheel /> Chromatacia
-                    </Heading>
-                </Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse className="justify-content-end">
-                    <Nav.Link href="/about">About</Nav.Link>
-                </Navbar.Collapse>
-            </Navbar>
-        </>
-    }
-}
+type NavigationProps = { };
+
+const Navigation: React.FC<NavigationProps> = ({
+
+}: NavigationProps) => {
+    const history = useHistory();
+
+    return (
+        <Navbar bg="dark" expand="lg" variant="dark">
+            <Navbar.Brand onClick={() => history.push('/')}>
+                <Heading size={600}>
+                    <ColorWheel /> Chromatacia
+                </Heading>
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse className="justify-content-end">
+                <Nav.Link 
+                    className="about-link" 
+                    onClick={() => history.push('/about')}
+                >About</Nav.Link>
+            </Navbar.Collapse>
+        </Navbar>
+    );
+};
+
+export default Navigation;
