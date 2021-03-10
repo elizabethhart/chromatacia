fbd281057ff4051a5d5632ebd68e6dfd12306e22
elizabethhart <elizabethwhart@gmail.com>
2021-03-09 21:57:00 -0600

    Add GA package

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import ReactGA from "react-ga";
 import Navigation from "./components/Navigation/Navigation";
 import Home from "./components/Home/Home";
 import About from "./components/About/About";
@@ -9,21 +10,23 @@ import HighSaffron from "./components/HighSaffron/HighSaffron";
 
 import "./App.scss";
 
-class App extends React.Component {
-  render() {
-    return (
-      <Router>
-        <Navigation />
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/about" component={About} />
-          <Route path="/gallery" component={Gallery} />
-          <Route path="/bookshelf" component={Bookshelf} />
-          <Route path="/highsaffron" component={HighSaffron} />
-        </Switch>
-      </Router>
-    );
+function App() {
+  if (process.env.REACT_APP_GOOGLE_ANALYTICS_TRACKING_ID) {
+    ReactGA.initialize(process.env.REACT_APP_GOOGLE_ANALYTICS_TRACKING_ID);
   }
+
+  return (
+    <Router>
+      <Navigation />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/gallery" component={Gallery} />
+        <Route path="/bookshelf" component={Bookshelf} />
+        <Route path="/highsaffron" component={HighSaffron} />
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
