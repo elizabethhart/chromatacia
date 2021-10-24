b7ae49035f35b5e91854ef3dcf1e25682fae80ae
elizabethhart <elizabethwhart@gmail.com>
2021-10-24 14:49:33 -0500

    Clean up dependencies

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
 import { Col, Container, Row } from "react-bootstrap";
-import styled from "styled-components";
 import { FaGithub, FaMailBulk, FaMapPin } from "react-icons/fa";
 import "./About.scss";
 
@@ -10,29 +9,15 @@ type AboutProps = {};
 const About: React.FC<AboutProps> = () => {
   const { t } = useTranslation();
 
-  const Card = styled.div`
-    background-color: #202020;
-    border-radius: 5px;
-    padding: 10px;
-    text-align: center;
-  `;
-
-  const ProfileImage = styled.img`
-    display: block;
-    width: 90%;
-    border-radius: 50%;
-    margin: 20px;
-    vertical-align: top;
-  `;
-
   return (
     <>
       <div className="about color-bar"></div>
       <Container className="about-container">
         <Row>
           <Col md="3">
-            <Card>
-              <ProfileImage
+            <div className="card">
+              <img
+                className="profile-image"
                 alt="google-map"
                 src="https://elizabethhart.github.io/images/IMG_1321.JPG"
               />
@@ -55,7 +40,7 @@ const About: React.FC<AboutProps> = () => {
               >
                 <FaMailBulk /> {"email"}
               </a>
-            </Card>
+            </div>
           </Col>
         </Row>
       </Container>
